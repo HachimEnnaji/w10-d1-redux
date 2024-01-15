1c6e3c0a9a7240b960269be1e7f31f84cb615441
HachimEnnaji <ashennaji@hotmail.it>
2024-01-15 16:45:42 +0100

    second commit

--- src/components/MainSearch.jsx
+++ src/components/MainSearch.jsx
@@ -1,51 +1,56 @@
-import { Row, Col } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import { Star, StarFill } from "react-bootstrap-icons";
-import { useSelector, useDispatch } from "react-redux";
+import { useState } from "react";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import Job from "./Job";
 
-const Job = ({ data }) => {
-  const favourites = useSelector((state) => state.favourite.list);
-  const dispatch = useDispatch();
+const MainSearch = () => {
+  const [query, setQuery] = useState("");
+  const [jobs, setJobs] = useState([]);
 
-  const isFav = favourites.includes(data.company_name);
+  const navigate = useNavigate();
+
+  const baseEndpoint = "https://strive-benchmark.herokuapp.com/api/jobs?search=";
+
+  const handleChange = (e) => {
+    setQuery(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(baseEndpoint + query + "&limit=20");
+      if (response.ok) {
+        const { data } = await response.json();
+        setJobs(data);
+      } else {
+        alert("Error fetching results");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
-    <Row className="mx-0 mt-3 p-3" style={{ border: "1px solid #00000033", borderRadius: 4 }}>
-      <Col xs={3}>
-        {isFav ? (
-          <StarFill
-            color="gold"
-            size={16}
-            className="mr-2 my-auto"
-            onClick={() =>
-              dispatch({
-                type: "REMOVE_FROM_FAVOURITE",
-                payload: data.company_name,
-              })
-            }
-          />
-        ) : (
-          <Star
-            color="gold"
-            size={16}
-            className="mr-2 my-auto"
-            onClick={() =>
-              dispatch({
-                type: "ADD_TO_FAVOURITE",
-                payload: data.company_name,
-              })
-            }
-          />
-        )}
-        <Link to={`/${data.company_name}`}>{data.company_name}</Link>
-      </Col>
-      <Col xs={9}>
-        <a href={data.url} target="_blank" rel="noreferrer">
-          {data.title}
-        </a>
-      </Col>
-    </Row>
+    <Container>
+      <Row>
+        <Col xs={10} className="mx-auto my-3">
+          <h1>Remote Jobs Search</h1>
+          <Button onClick={() => navigate("/favourites")}>Favourites</Button>
+        </Col>
+        <Col xs={10} className="mx-auto">
+          <Form onSubmit={handleSubmit}>
+            <Form.Control type="search" value={query} onChange={handleChange} placeholder="type and press Enter" />
+          </Form>
+        </Col>
+        <Col xs={10} className="mx-auto mb-5">
+          {jobs.map((jobData) => (
+            <Job key={jobData._id} data={jobData} />
+          ))}
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
-export default Job;
+export default MainSearch;
